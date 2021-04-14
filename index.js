ca747fe633f4ce4b1975336f7a8f97682cf2da78
KPS53 <70844306+KPS53@users.noreply.github.com>
2021-04-14 15:32:23 +0200

    Fixed a little issue

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function replace(data, replaced) {
     if (!data) throw Error(`You must provide a data.`);
     if (!replaced) throw Error(`You must provide a replaced object.`)
     for (w in replaced) {
-        data = data.replace(variable, replaced[w])
+        data = data.replace(data, replaced[w])
     }
     return data
 }
