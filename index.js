f85719d50d5b65fe9622a960d70d0ce9336aa2b9
KPS53 <70844306+KPS53@users.noreply.github.com>
2021-04-14 15:00:33 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * github.com/KPS53
+ * @param {string} data [String which contains the values that have to be replaced]
+ * @param {object} replaced [Object which has one or several key(s) (to be replaced) and their value (replaced by)]
+ *
+ *
+ */
+
+function replace(data, replaced) {
+    if (!data) throw Error(`You must provide a data.`);
+    if (!replaced) throw Error(`You must provide a replaced object.`)
+    for (w in replaced) {
+        data = data.replace(variable, replaced[w])
+    }
+    return data
+}
+
+module.exports = replace
