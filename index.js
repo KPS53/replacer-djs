6b967ac2583ece9b90458a78b3323167e26c0e5e
Raphael <veauville.raphael@gmail.com>
2021-05-29 10:44:38 +0200

    Fix error of the module which returns only the last word replaced

--- index.js
+++ index.js
@@ -9,8 +9,9 @@
 function replace(data, replaced) {
     if (!data) throw Error(`You must provide a data.`);
     if (!replaced) throw Error(`You must provide a replaced object.`)
-    for (w in replaced) {
-        data = data.replace(data, replaced[w])
+    const keys = Object.keys(replaced)
+    for (w of keys) {
+        data = data.replace(w, replaced[w])
     }
     return data
 }
